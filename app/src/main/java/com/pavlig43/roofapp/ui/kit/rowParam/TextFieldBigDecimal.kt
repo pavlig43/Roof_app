89ac39584f33c600128eee191656ce7114358193
user <pavlig43@gmail.com>
2024-11-26 20:49:43 +0300

    Added dynamic placeholder to TextField that changes based on focus state for BigDecimal input

--- app/src/main/java/com/pavlig43/roofapp/ui/kit/rowParam/TextFieldBigDecimal.kt
+++ app/src/main/java/com/pavlig43/roofapp/ui/kit/rowParam/TextFieldBigDecimal.kt
@@ -1,10 +1,19 @@
 package com.pavlig43.roofapp.ui.kit.rowParam
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import com.pavlig43.roofapp.ui.theme.Roof_appTheme
 import java.math.BigDecimal
 
 @Composable
@@ -14,14 +23,36 @@ fun TextFieldBigDecimal(
     modifier: Modifier = Modifier,
 
 ) {
-    TextField(
-        value = value.takeIf { it != BigDecimal.ZERO }?.toPlainString() ?: "",
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val placeholder by remember(isFocused) { mutableStateOf(if (isFocused) "" else "0") }
 
-        onValueChange = { input ->
-            updateParam(input.toBigDecimalOrNull() ?: BigDecimal.ZERO)
-        },
-        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-        modifier = modifier
+    Column {
+        TextField(
+            value = value.takeIf { it != BigDecimal.ZERO }?.toPlainString() ?: "",
 
-    )
+            onValueChange = { input ->
+                if (input.toIntOrNull() != null) {
+                    updateParam(input.toBigDecimalOrNull() ?: BigDecimal.ZERO)
+                }
+            },
+            placeholder = { Text(placeholder) },
+            interactionSource = interactionSource,
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            modifier = modifier
+
+        )
+    }
+}
+
+@Suppress("UnusedPrivateMember")
+@Preview(showBackground = true)
+@Composable
+private fun TextFieldBigDecimalPreview() {
+    Roof_appTheme {
+        TextFieldBigDecimal(
+            value = BigDecimal.ZERO,
+            updateParam = {}
+        )
+    }
 }
